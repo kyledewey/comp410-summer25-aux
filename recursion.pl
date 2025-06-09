7d800983c3d05210c21ab2caeb35ba32a3b9e9f4
Kyle Dewey <kyle.dewey@csun.edu>
2025-06-09 11:34:27 -0700

    .

--- recursion.pl
+++ recursion.pl
@@ -7,4 +7,16 @@ myLength(cons(_, SmallerList), OutputLength) :-
     myLength(SmallerList, SmallerListLength),
     OutputLength is SmallerListLength + 1.
 
+factorial(0, 1).
+factorial(N, X) :-
+    N > 0,
+    SmallerN is N - 1,
+    factorial(SmallerN, Rest),
+    X is N * Rest.
     
+myBetween(Min, Max, Min) :-
+    Min =< Max.
+myBetween(Min, Max, Ret) :-
+    Min =< Max,
+    LargerMin is Min + 1,
+    myBetween(LargerMin, Max, Ret).
