d9f0e56c46cd65e8efd3145e615dbc9cbd3ce6ac
Kyle Dewey <kyle.dewey@csun.edu>
2025-06-10 11:38:09 -0700

    .

--- recursion.pl
+++ recursion.pl
@@ -7,6 +7,18 @@ myLength(cons(_, SmallerList), OutputLength) :-
     myLength(SmallerList, SmallerListLength),
     OutputLength is SmallerListLength + 1.
 
+myAppend(nil, List, List).
+myAppend(cons(Head, Tail), OtherList, cons(Head, RecResult)) :-
+    myAppend(Tail, OtherList, RecResult).
+
+% myAppend(InputList1, InputList2, OutputList)
+%% myAppend(nil, InputList, OutputList) :-
+%%     OutputList = InputList.
+% myAppend(List, nil, List).
+%% myAppend(cons(Head, Tail), OtherList, OutputList) :-
+%%     OutputList = cons(Head, RecResult),
+%%     myAppend(Tail, OtherList, RecResult).
+
 factorial(0, 1).
 factorial(N, X) :-
     N > 0,
